1f3e286e1267f1111cb5d561d5c47284995cd8f9
Grreku <michal.basiuras@gmail.com>
2018-09-12 00:10:23 +0200

    Update image.py
    ma dzialac

--- image.py
+++ image.py
@@ -5,23 +5,6 @@ import datetime
 
 app = Flask(__name__)
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-
-SIMPLE_DB_DOMAIN_NAME = os.environ['SIMPLE_DB_DOMAIN_NAME']
-
-QUEUE_NAME = os.environ['QUEUE_NAME']
-BUCKET_NAME = os.environ['BUCKET_NAME']
-BUCKET_URL = 'https://s3-us-west-2.amazonaws.com/{}'.format(BUCKET_NAME)
-
-#s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-#sqs = boto3.resource('sqs', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY_ID,
-#                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-#queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
-#
-#sdb = boto3.client('sdb', region_name='us-west-2', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -32,7 +15,7 @@ def done():
 
 @app.route('/choose')
 def modify():
-    client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-west-2')
+    client = boto3.client('s3', region_name='us-west-2')
     response = client.list_objects_v2(
     Bucket = 'projekt-psoir1',
     Prefix = 'uploads/'
@@ -46,9 +29,9 @@ def modify():
 @app.route('/queue', methods=["POST"])
 def queue():
     image_list = request.form.getlist("uploads")
-    sqs = boto3.resource("sqs", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-west-2')
+    sqs = boto3.resource("sqs", region_name='us-west-2')
     queue = sqs.get_queue_by_name(QueueName="projekt-psoir-queue")
-    simpledb = boto3.client("sdb", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-west-2')
+    simpledb = boto3.client("sdb", region_name='us-west-2')
     for image_name in image_list:
         queue.send_message(MessageBody="{0}".format(image_name))
         simpledb.put_attributes(DomainName="PsoirSimpleDB", ItemName="Image",
